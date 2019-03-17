9353aea7199fda89c39bb03414a303d3ae594e0a
Ryan Fauver <ryan.fauver@gmail.com>
2019-03-16 18:52:46 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'nokogiri'                # an HTML, XML, SAX, and Reader parser
+
+group :development, :test do
+  gem 'dotenv'                # loads environment variables from `.env`.
+end
---
+++ notification.rb
@@ -0,0 +1,31 @@
+require 'dotenv/load'
+require 'nokogiri'
+require 'open-uri'
+require 'net/https'
+
+class Notifier
+  URL = URI.parse("https://api.pushover.net/1/messages.json")
+
+  def self.call(message:)
+    request = Net::HTTP::Post.new(URL.path)
+    request.set_form_data(
+      token: ENV['PUSHOVER_TOKEN'],
+      user: ENV['PUSHOVER_USER'],
+      message: message
+    )
+    res = Net::HTTP.new(URL.host, URL.port)
+    res.use_ssl = true
+    res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    res.start { |http| http.request(request) }
+  end
+end
+
+page = Nokogiri::HTML(open("https://touchstoneclimbing.com/gwpower-co/route-setting/"))
+keys = [:location, :date, :problems]
+row = page.css('.table-routes tbody tr:first-child td').map(&:text)
+row = keys.zip(row).to_h
+# today_string = Time.now.strftime('%-m/%d')
+# row[:date] == today_string
+Notifier.call(
+  message: "New Routes at Great Western Power Co.\n#{row[:problems]} in the #{row[:location]}"
+)
